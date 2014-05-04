2e78cbb85a75b85de4a04e367f8753db7b87fcd7
Casey Scarborough <caseyscarborough@gmail.com>
2014-05-04 16:19:12 -0400

    Update plugin descriptor.

--- NexmoGrailsPlugin.groovy
+++ NexmoGrailsPlugin.groovy
@@ -4,37 +4,25 @@ class NexmoGrailsPlugin {
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "2.3 > *"
   // resources that are excluded from plugin packaging
-  def pluginExcludes = [
-      "grails-app/views/error.gsp"
-  ]
+  def pluginExcludes = [ ]
 
-  // TODO Fill in these fields
   def title = "Nexmo Plugin" // Headline display name of the plugin
-  def author = "Your name"
-  def authorEmail = ""
+  def author = "Casey Scarborough"
+  def authorEmail = "caseyscarborough@gmail.com"
   def description = '''\
-Brief summary/description of the plugin.
+The Nexmo Plugin gives applications SMS (Text Messaging) and Voice functionality. It allows an application to receive
+and send text messages, as well as sending automated voice calls.
 '''
 
   // URL to the plugin's documentation
-  def documentation = "http://grails.org/plugin/nexmo"
-
-  // Extra (optional) plugin metadata
-
-  // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-  // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-  // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+  def documentation = "https://github.com/caseyscarborough/nexmo"
+  def license = "APACHE"
 
   // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+  def issueManagement = [ system: "GitHub", url: "https://github.com/caseyscarborough/nexmo/issues" ]
 
   // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+  def scm = [ url: "https://github.com/caseyscarborough/nexmo" ]
 
   def doWithWebDescriptor = { xml ->
     // TODO Implement additions to web.xml (optional), this event occurs before
