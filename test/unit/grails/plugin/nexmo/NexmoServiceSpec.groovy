b78d7060f9bc524ffa2aeba02ba13a27b9293be7
Casey Scarborough <caseyscarborough@gmail.com>
2014-05-06 23:17:18 -0400

    Add test for sending SMS.

---
+++ test/unit/grails/plugin/nexmo/NexmoServiceSpec.groovy
@@ -0,0 +1,27 @@
+package grails.plugin.nexmo
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(NexmoService)
+class NexmoServiceSpec extends Specification {
+
+  def setup() {
+    def config = new ConfigSlurper().parse(new File('grails-app/conf/NexmoConfig.groovy').toURL())
+    grailsApplication.config.nexmo = config.nexmo
+    assert grailsApplication.config.nexmo.test.phone_number
+    service.grailsApplication = grailsApplication
+  }
+
+  def cleanup() {
+  }
+
+  void "Test Successful SMS"() {
+    when:
+    def result = service.sendSms(grailsApplication.config?.nexmo?.test?.phone_number, "This is a test from Grails!")
+
+    then:
+    result['message-count'] == "1"
+    result.messages[0].status == "0"
+  }
+}
