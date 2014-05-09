22b5893e5a2250515e139ded0d4ed0d6be517ac6
Casey Scarborough <caseyscarborough@gmail.com>
2014-05-09 12:04:07 -0400

    Implement sending phone calls using text to speech.

--- grails-app/services/grails/plugin/nexmo/NexmoService.groovy
+++ grails-app/services/grails/plugin/nexmo/NexmoService.groovy
@@ -28,7 +28,7 @@ class NexmoService {
         def message = data?.messages[0]
         def statusCode = message?.status
         if (statusCode != "0") {
-          def error = message?."error-text" ?: getMessage(statusCode, "An error occurred sending the SMS.")
+          def error = "${message?."error-text"}: ${getMessage("sms", statusCode, "An error occurred sending the SMS.")}"
           log.error("An error occurred sending the SMS: ${error}")
           throw new NexmoException(error)
         }
@@ -42,13 +42,43 @@ class NexmoService {
     }
   }
 
+  def call(String to, String text, String from="") {
+    if (!to || !text) {
+      log.error("A required parameter was not supplied. Not making.")
+      throw new NexmoException("A required parameter was not supplied. Not making call.")
+    }
+
+    def http = new HTTPBuilder(config?.endpoint)
+    def requestBody = [to: to, text: text, from: from, api_key: config?.api?.key, api_secret: config?.api?.secret]
+
+    http.request(POST) {
+      uri.path = "/tts/${config?.format}"
+      send(URLENC, requestBody)
+
+      response.success = { resp, data ->
+        def statusCode = data?.status
+        if (statusCode != "0") {
+          def error = "${data?."error-text"}: ${getMessage("call", statusCode, "An error occurred making the phone call.")}"
+          log.error("An error occurred making the call: ${error}")
+          throw new NexmoException(error)
+        }
+        log.info("Call was successfully sent.")
+        return [status: data?.status, id: data?."call-id"]
+      }
+      response.failure = { resp, data ->
+        log.error("An error occurred making the call.")
+        throw new NexmoException("A ${resp?.status} error was encountered.")
+      }
+    }
+  }
+
   private ConfigObject getConfig() {
     return grailsApplication.config?.nexmo
   }
 
-  private String getMessage(String code, String defaultMessage) {
+  private String getMessage(String type, String code, String defaultMessage) {
     if (code) {
-      return messageSource.getMessage("nexmo.sms.status.${code}", [].toArray(), defaultMessage, LocaleContextHolder.locale)
+      return messageSource.getMessage("nexmo.${type}.status.${code}", [].toArray(), defaultMessage, LocaleContextHolder.locale)
     }
     return defaultMessage
   }
